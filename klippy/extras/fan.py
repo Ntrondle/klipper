81f3936395fd5d895cbe088ba0b0fe8a3f0d5e36
Absak01 <94861272+Ntrondle@users.noreply.github.com>
2025-05-29 19:41:11 +0200

    Update fan.py

--- klippy/extras/fan.py
+++ klippy/extras/fan.py
@@ -1,3 +1,28 @@
+class StandaloneWheelSensor:
+    def __init__(self, config):
+        self.name = config.get_name().split()[-1]
+        printer = config.get_printer()
+        self._freq_counter = None
+
+        pin = config.get('pin', None)
+        if pin is not None:
+            self.ppr = config.getint('pulses_per_rev', 6, minval=1)
+            poll_time = config.getfloat('poll_interval', 0.0015, above=0.)
+            sample_time = config.getfloat('sample_time', 0.2, above=0.01)
+            self._freq_counter = pulse_counter.FrequencyCounter(
+                printer, pin, sample_time, poll_time)
+
+        printer.add_object(f"{self.name}_sensor", self)
+
+    def get_rpm(self):
+        if self._freq_counter is not None:
+            return self._freq_counter.get_frequency() * 30. / self.ppr
+        return None
+
+    def get_status(self, eventtime):
+        return {
+            'rpm': self.get_rpm()
+        }
 # Printer cooling fan
 #
 # Copyright (C) 2016-2024  Kevin O'Connor <kevin@koconnor.net>
@@ -80,6 +105,7 @@ class Fan:
             'rpm': tachometer_status['rpm'],
         }
 
+
 class FanTachometer:
     def __init__(self, config):
         printer = config.get_printer()
@@ -101,6 +127,28 @@ class FanTachometer:
             rpm = None
         return {'rpm': rpm}
 
+# WheelTachometer class with configurable sample time and get_rpm() method
+class WheelTachometer:
+    def __init__(self, config):
+        printer = config.get_printer()
+        self._freq_counter = None
+
+        pin = config.get('tachometer_pin', None)
+        if pin is not None:
+            self.ppr = config.getint('tachometer_ppr', 6, minval=1)
+            poll_time = config.getfloat('tachometer_poll_interval', 0.0015, above=0.)
+            sample_time = config.getfloat('tachometer_sample_time', 0.2, above=0.01)
+            self._freq_counter = pulse_counter.FrequencyCounter(
+                printer, pin, sample_time, poll_time)
+
+    def get_rpm(self):
+        if self._freq_counter is not None:
+            return self._freq_counter.get_frequency() * 30. / self.ppr
+        return None
+
+    def get_status(self, eventtime):
+        return {'rpm': self.get_rpm()}
+
 class PrinterFan:
     def __init__(self, config):
         self.fan = Fan(config)
@@ -120,3 +168,7 @@ class PrinterFan:
 
 def load_config(config):
     return PrinterFan(config)
+
+# Registration function for StandaloneWheelSensor
+def load_config_prefix(config):
+    return WheelTachometer(config)
