ee1d373831ab9e582ffb97c7374a9498af10d058
Absak01 <94861272+Ntrondle@users.noreply.github.com>
2025-05-29 21:36:56 +0200

    Update wheel_sensor.py

--- klippy/extras/wheel_sensor.py
+++ klippy/extras/wheel_sensor.py
@@ -3,6 +3,8 @@
 # new "tachometer_pin"/"tachometer_ppr"/"tachometer_poll_interval"/"tachometer_sample_time" keys for configuration compatibility.
 
 from . import pulse_counter, output_pin
+import logging
+LOG = logging.getLogger("wheel_sensor")
 
 class StandaloneWheelSensor:
     def __init__(self, config):
@@ -12,7 +14,9 @@ class StandaloneWheelSensor:
 
         # Accept both "pin" and "tachometer_pin" for compatibility
         pin = config.get('pin', config.get('tachometer_pin', None))
-        if pin is not None:
+        if pin is None:
+            LOG.warning("wheel_sensor '%s': no pin specified; tachometer disabled", self.name)
+        else:
             # pulses‑per‑rev: allow both styles
             self.ppr = config.getint('pulses_per_rev',
                                      config.getint('tachometer_ppr', 1),
@@ -25,17 +29,22 @@ class StandaloneWheelSensor:
                           above=0.01)
             self._freq_counter = pulse_counter.FrequencyCounter(
                 printer, pin, sample_time, poll_time)
+            LOG.info("wheel_sensor '%s': using pin %s ppr=%s poll=%s sample=%s",
+                     self.name, pin, self.ppr, poll_time, sample_time)
 
         printer.add_object(self.name, self)
 
     def get_rpm(self):
+        rpm = None
         if self._freq_counter is not None:
-            return self._freq_counter.get_frequency() * 30. / self.ppr
-        return None
+            rpm = self._freq_counter.get_frequency() * 30. / self.ppr
+            LOG.debug("wheel_sensor '%s': rpm=%.2f", self.name, rpm)
+        return rpm
 
     def get_status(self, eventtime):
+        rpm = self.get_rpm()
         return {
-            'rpm': self.get_rpm()
+            'rpm': rpm
         }
 
 def load_config_prefix(config):
