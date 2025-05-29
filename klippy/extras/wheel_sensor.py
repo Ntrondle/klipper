f7aac89977943e48c51a1127bb5aa988b96a7b18
Absak01 <94861272+Ntrondle@users.noreply.github.com>
2025-05-29 20:29:00 +0200

    Update wheel_sensor.py

--- klippy/extras/wheel_sensor.py
+++ klippy/extras/wheel_sensor.py
@@ -11,7 +11,8 @@ class StandaloneWheelSensor:
             self.ppr = config.getint('pulses_per_rev', 6, minval=1)
             poll_time = config.getfloat('poll_interval', 0.0015, above=0.)
             sample_time = config.getfloat('sample_time', 0.2, above=0.01)
-            self._freq_counter = printer.load_object(config, 'pulse_counter').FrequencyCounter(
+            pulse_counter_module = printer.load_object(config, 'pulse_counter')
+            self._freq_counter = pulse_counter_module.FrequencyCounter(
                 printer, pin, sample_time, poll_time)
 
         printer.add_object(f"{self.name}_sensor", self)
